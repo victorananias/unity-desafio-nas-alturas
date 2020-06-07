3ffcec2d51dda1ff5df9a176f2ecc92a27ad90d5
Victor <victor@ananias.dev>
2020-06-07 15:02:08 -0300

    commit inicial - fim aula 1

---
+++ Assets/Scripts/AviaoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AviaoController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Impulsionar();
+        }
+    }
+
+    private void Impulsionar()
+    {
+        _rigidbody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+}
