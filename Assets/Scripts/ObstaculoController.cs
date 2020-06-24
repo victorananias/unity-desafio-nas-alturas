0ab6815f18038e6266ecdf3832bc23fe2d55dc24
Victor <victor@ananias.dev>
2020-06-24 13:38:47 -0300

    obstáculo será removido após sumir da tela

--- Assets/Scripts/ObstaculoController.cs
+++ Assets/Scripts/ObstaculoController.cs
@@ -1,12 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ObstaculoController : MonoBehaviour
 {
-    [SerializeField]
-    private float velocidade = 5f;
+    [SerializeField] private float velocidade = 5f;
 
-    [SerializeField] 
-    private float variacaoY;
+    [SerializeField] private float variacaoY;
 
     private void Awake()
     {
@@ -14,13 +14,23 @@ public class ObstaculoController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Mover();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destruir();
     }
 
     private void Mover()
     {
         transform.Translate(Vector3.left * (velocidade * Time.deltaTime));
+    }
+
+    private void Destruir()
+    {
+        Destroy(gameObject);
     }
 }
