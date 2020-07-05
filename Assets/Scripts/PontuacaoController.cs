09b0f04fb433b485fddb07da1926c39f4890bd20
Victor <victor@ananias.dev>
2020-07-05 13:53:03 -0300

    audio adicionado ao jogo

--- Assets/Scripts/PontuacaoController.cs
+++ Assets/Scripts/PontuacaoController.cs
@@ -9,12 +9,19 @@ public class PontuacaoController : MonoBehaviour
     [SerializeField]
     private Text textPontuacao;
     private int pontos;
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
 
 
     public void AdicionarPontos()
     {
         pontos++;
         AtualizarTexto();
+        audio.Play();
     }
 
     public void Reiniciar()
